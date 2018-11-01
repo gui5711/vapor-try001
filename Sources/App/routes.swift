cd19a93fe76cb1a269855891b2bda73d90adef12
Guilherme Fischer <1999.gfischer@gmail.com>
2018-11-01 15:15:32 -0300

    Servidor WebSocket

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,14 +3,67 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
+//    router.get { req in
+//        return "It works!"
+//    }
     
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    // First, create an HTTPProtocolUpgrader
+    let ws = HTTPServer.webSocketUpgrader(shouldUpgrade: { req in
+        // Returning nil in this closure will reject upgrade
+        if req.url.path == "/deny" { return nil }
+        // Return any additional headers you like, or just empty
+        return [:]
+    }, onUpgrade: { ws, req in
+        // This closure will be called with each new WebSocket client
+        ws.send("Connected")
+        ws.onText { ws, string in
+            ws.send(string.reversed())
+        }
+    })
+    
+    /// Echoes the request as a response.
+    struct EchoResponder: HTTPServerResponder {
+        /// See `HTTPServerResponder`.
+        func respond(to req: HTTPRequest, on worker: Worker) -> Future<HTTPResponse> {
+            // Create an HTTPResponse with the same body as the HTTPRequest
+            let res = HTTPResponse(body: req.body)
+            // We don't need to do any async work here, we can just
+            // se the Worker's event-loop to create a succeeded future.
+            return worker.eventLoop.newSucceededFuture(result: res)
+        }
+    }
+    
+    // Create an EventLoopGroup with an appropriate number
+    // of threads for the system we are running on.
+    let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+    // Make sure to shutdown the group when the application exits.
+    defer { try! group.syncShutdownGracefully() }
+    
+    // Start an HTTPServer using our EchoResponder
+    // We are fine to use `wait()` here since we are on the main thread.
+    let server = try HTTPServer.start(
+        hostname: "localhost",
+        port: 8080,
+        responder: EchoResponder(),
+        upgraders: [ws],
+        on: group
+        ).wait()
+    
+    // Wait for the server to close (indefinitely).
+    try server.onClose.wait()
+    
+    //    router.websocket("ping") { (req, ws) in
+    //        ws.onString({ (ws, msg) in
+    //            if msg == "ping" {
+    //                ws.send(string: "pong")
+    //           }
+    //        })
+    //    }
 
     // Example of configuring a controller
     let todoController = TodoController()
